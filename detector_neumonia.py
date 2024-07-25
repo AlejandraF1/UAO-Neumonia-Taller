c0b6da057d71492e5b586f3fd425d2a785796812
AlejandraF1 <alejandra.florez_mal@uao.edu.co>
2024-07-25 10:11:32 -0500

    correción de indentación

--- detector_neumonia.py
+++ detector_neumonia.py
@@ -125,81 +125,82 @@ class App:
 
 if __name__ == "__main__":
     my_app = App()
-    my_app.root.mainloop()    
+    my_app.root.mainloop()
 
-        # BOLD FONT
-        fonti = font.Font(weight="bold")
+# BOLD FONT
+fonti = font.Font(weight="bold")
 
-        self.root.geometry("815x560")
-        self.root.resizable(0, 0)
+self.root.geometry("815x560")
+self.root.resizable(0, 0)
 
-        # LABELS
-        self.lab1 = ttk.Label(self.root, text="Imagen Radiográfica", font=fonti)
-        self.lab2 = ttk.Label(self.root, text="Imagen con Heatmap", font=fonti)
-        self.lab3 = ttk.Label(self.root, text="Resultado:", font=fonti)
-        self.lab4 = ttk.Label(self.root, text="Cédula Paciente:", font=fonti)
-        self.lab5 = ttk.Label(
-            self.root,
-            text="SOFTWARE PARA EL APOYO AL DIAGNÓSTICO MÉDICO DE NEUMONÍA",
-            font=fonti,
-        )
-        self.lab6 = ttk.Label(self.root, text="Probabilidad:", font=fonti)
+# LABELS
+self.lab1 = ttk.Label(self.root, text="Imagen Radiográfica", font=fonti)
+self.lab2 = ttk.Label(self.root, text="Imagen con Heatmap", font=fonti)
+self.lab3 = ttk.Label(self.root, text="Resultado:", font=fonti)
+self.lab4 = ttk.Label(self.root, text="Cédula Paciente:", font=fonti)
+self.lab5 = ttk.Label(
+    self.root,
+    text="SOFTWARE PARA EL APOYO AL DIAGNÓSTICO MÉDICO DE NEUMONÍA",
+    font=fonti,
+)
+self.lab6 = ttk.Label(self.root, text="Probabilidad:", font=fonti)
 
-        # TWO STRING VARIABLES TO CONTAIN ID AND RESULT
-        self.ID = tk.StringVar()
-        self.result = tk.StringVar()
+# TWO STRING VARIABLES TO CONTAIN ID AND RESULT
+self.ID = tk.StringVar()
+self.result = tk.StringVar()
 
-        # TWO INPUT BOXES
-        self.text1 = ttk.Entry(self.root, textvariable=self.ID, width=10)
+# TWO INPUT BOXES
+self.text1 = ttk.Entry(self.root, textvariable=self.ID, width=10)
 
-        # TWO IMAGE INPUT BOXES
-        self.text_img1 = tk.Text(self.root, width=31, height=15)
-        self.text_img2 = tk.Text(self.root, width=31, height=15)
-        self.text2 = tk.Text(self.root)
-        self.text3 = tk.Text(self.root)
+# TWO IMAGE INPUT BOXES
+self.text_img1 = tk.Text(self.root, width=31, height=15)
+self.text_img2 = tk.Text(self.root, width=31, height=15)
+self.text2 = tk.Text(self.root)
+self.text3 = tk.Text(self.root)
 
-        # BUTTONS
-        self.button1 = ttk.Button(
-            self.root, text="Predecir", state="disabled", command=self.run_model
-        )
-        self.button2 = ttk.Button(
-            self.root, text="Cargar Imagen", command=self.load_img_file
-        )
-        self.button3 = ttk.Button(self.root, text="Borrar", command=self.delete)
-        self.button4 = ttk.Button(self.root, text="PDF", command=self.create_pdf)
-        self.button6 = ttk.Button(
-            self.root, text="Guardar", command=self.save_results_csv
-        )
+# BUTTONS
+self.button1 = ttk.Button(
+    self.root, text="Predecir", state="disabled", command=self.run_model
+)
+self.button2 = ttk.Button(
+    self.root, text="Cargar Imagen", command=self.load_img_file
+)
+self.button3 = ttk.Button(self.root, text="Borrar", command=self.delete)
+self.button4 = ttk.Button(self.root, text="PDF", command=self.create_pdf)
+self.button6 = ttk.Button(
+    self.root, text="Guardar", command=self.save_results_csv
+)
 
-        # WIDGETS POSITIONS
-        self.lab1.place(x=110, y=65)
-        self.lab2.place(x=545, y=65)
-        self.lab3.place(x=500, y=350)
-        self.lab4.place(x=65, y=350)
-        self.lab5.place(x=122, y=25)
-        self.lab6.place(x=500, y=400)
-        self.button1.place(x=220, y=460)
-        self.button2.place(x=70, y=460)
-        self.button3.place(x=670, y=460)
-        self.button4.place(x=520, y=460)
-        self.button6.place(x=370, y=460)
-        self.text1.place(x=200, y=350)
-        self.text2.place(x=610, y=350, width=90, height=30)
-        self.text3.place(x=610, y=400, width=90, height=30)
-        self.text_img1.place(x=65, y=90)
-        self.text_img2.place(x=500, y=90)
+# WIDGETS POSITIONS
+self.lab1.place(x=110, y=65)
+self.lab2.place(x=545, y=65)
+self.lab3.place(x=500, y=350)
+self.lab4.place(x=65, y=350)
+self.lab5.place(x=122, y=25)
+self.lab6.place(x=500, y=400)
+self.button1.place(x=220, y=460)
+self.button2.place(x=70, y=460)
+self.button3.place(x=670, y=460)
+self.button4.place(x=520, y=460)
+self.button6.place(x=370, y=460)
+self.text1.place(x=200, y=350)
+self.text2.place(x=610, y=350, width=90, height=30)
+self.text3.place(x=610, y=400, width=90, height=30)
+self.text_img1.place(x=65, y=90)
+self.text_img2.place(x=500, y=90)
 
-        # FOCUS ON PATIENT ID
-        self.text1.focus_set()
+# FOCUS ON PATIENT ID
+self.text1.focus_set()
 
-        # INSTANCE OF DETECTOR
-        self.detector = DetectorNeumonia()
+# INSTANCE OF DETECTOR
+self.detector = DetectorNeumonia()
 
-        # NUMERO DE IDENTIFICACIÓN PARA GENERAR PDF
-        self.reportID = 0
+# NUMERO DE IDENTIFICACIÓN PARA GENERAR PDF
+self.reportID = 0
 
-        # RUN LOOP
-        self.root.mainloop()
+# RUN LOOP
+self.root.mainloop()
+
 
     # METHODS
     def load_img_file(self):
